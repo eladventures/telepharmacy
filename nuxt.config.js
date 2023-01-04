a1c68a31865ebc757b25e6cef6c1500f96cecade
Joff Tiquez <jofftiquez@gmail.com>
2023-01-04 15:48:05 +0800

    chore: committing is not a crime

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
+export default defineNuxtConfig({
+  preset: 'node-server',
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  nitro: {
+    preset: 'firebase',
+  },
+});
